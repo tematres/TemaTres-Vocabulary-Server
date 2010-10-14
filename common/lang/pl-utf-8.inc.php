9ab0fda5a9525294905339cceef28858abb44608
tematres <tematres@r020.com.ar>
2010-10-14 15:19:45 -0300

    add polsky
    Signed-off-by: tematres <tematres@r020.com.ar>

---
+++ common/lang/pl-utf-8.inc.php
@@ -0,0 +1,362 @@
+<?php
+#   TemaTres: open source thesaurus management #       #
+#                                                                        #
+#   Distribuido bajo Licencia GNU Public License, versión 2 (de junio de 1.991) Free Software Foundation
+#   Version 0.97
+#
+###############################################################################################################
+#
+define("LANG","pl");
+
+define("TR_acronimo","RT");
+define("TE_acronimo","NT");
+define("TG_acronimo","BT");
+define("UP_acronimo","UF");
+
+define("TR_termino","Termin powiązany");
+define("TE_termino","Termin węższy");
+define("TG_termino","Termin szerszy");
+define("UP_termino","Używany dla");
+/* v 9.5 */
+define("USE_termino","Użyj");
+
+define("MENU_ListaSis","Lista hierarchiczna");
+define("MENU_ListaAbc","Lista alfabetyczna");
+define("MENU_Sobre","O ...");
+define("MENU_Inicio","Home");
+
+define("MENU_MiCuenta","Moje konto");
+define("MENU_Usuarios","Użytkownicy");
+define("MENU_NuevoUsuario","Nowy użytkownik");
+define("MENU_DatosTesauro","O tezaurusie");
+
+define("MENU_AgregarT","Dodaj termin");
+define("MENU_EditT","Edytuj termin");
+define("MENU_BorrarT","Usuń termin");
+define("MENU_AgregarTG","Podporządkuj ten termin");
+define("MENU_AgregarTE","Subordinated Term");
+define("MENU_AgregarTR","Termin powiązany");
+define("MENU_AgregarUP","Termin równoznaczny");
+
+define("MENU_MisDatos","Moje konto");
+define("MENU_Caducar","Deaktywuj");
+define("MENU_Habilitar","Dostępne");
+define("MENU_Salir","Wyloguj");
+
+define("LABEL_Menu","Menu");
+define("LABEL_Opciones","Opcje");
+define("LABEL_Admin","Administracja");
+define("LABEL_Agregar","Dodaj");
+define("LABEL_editT","Edytuj termin");
+define("LABEL_EditorTermino","Edytor terminów");
+define("LABEL_Termino","Termin");
+define("LABEL_NotaAlcance","Nota o zakresie");
+define("LABEL_EliminarTE","Usuń termin");
+
+define("LABEL_AgregarT","Nowy termin");
+define("LABEL_AgregarTG","Dodaj szerszy termin do %s ");
+define("LABEL_AgregarTE","Nowy termin podporządkowny dla ");
+define("LABEL_AgregarUP","Nowy UF termin dla ");
+define("LABEL_AgregarTR","Nowy termin powiązany dla ");
+define("LABEL_Detalle","szczegóły");
+
+define("LABEL_Autor","Autor");
+define("LABEL_URI","URI");
+define("LABEL_Version","Powered by");
+define("LABEL_Idioma","Język");
+define("LABEL_Fecha","Data utworzenia");
+define("LABEL_Keywords","Słowa kluczowe");
+define("LABEL_TipoLenguaje","Typ języka");
+define("LABEL_Cobertura","Scope");
+define("LABEL_Terminos","terminy");
+define("LABEL_RelTerminos","relacje pomiędzy terminami");
+define("LABEL_TerminosUP","terminy równoznaczne");
+
+define("LABEL_BuscaTermino","Znajdź termin");
+define("LABEL_Buscar","Szukaj");
+define("LABEL_Enviar","Zatwierdź");
+define("LABEL_Cambiar","Aktualizuj");
+define("LABEL_Anterior","wstecz");
+define("LABEL_AdminUser","Administracja użytkownikami");
+define("LABEL_DatosUser","Dane użytkownika");
+define("LABEL_Acciones","Zadania");
+define("LABEL_verEsquema","Pokaż schemat");
+define("LABEL_actualizar","Aktualizuj");
+define("LABEL_terminosLibres","Wolne terminy");
+define("LABEL_busqueda","Szukaj");
+define("LABEL_borraRelacion","Usuń relację");
+define("MSG_ResultBusca","Znaleziono termin/ów ");
+define("MSG_ResultLetra","Liter");
+define("MSG_ResultCambios","zatwierdzono zmianę");
+define("MSG_noUser","<p>Niezarejestrowany użytkownik</p>");
+define("FORM_JS_check","Proszę sprawdzić datę ");
+define("FORM_JS_confirm","Usunąć tę relację?");
+define("FORM_JS_pass","_hasło");
+define("FORM_JS_confirmPass","_potwierdź_hasło");
+define("FORM_LABEL_termino","_termin");
+define("FORM_LABEL_buscar","_wyszukiwane_wyrażenie");
+define("FORM_LABEL_buscarTermino","_termin_powiązany");
+define("FORM_LABEL_nombre","_imię");
+define("FORM_LABEL_apellido","_nazwisko");
+define("FORM_LABEL_mail","_mail");
+define("FORM_LABEL_pass","_hasło");
+define("FORM_LABEL_repass","_potwierdź_hasło");
+define("FORM_LABEL_orga","orga");
+define("LABEL_nombre","imię");
+define("LABEL_apellido","nazwisko");
+define("LABEL_mail","mail");
+define("LABEL_pass","hasło");
+define("LABEL_repass","Potwierdź hasło");
+define("LABEL_orga","organizcja");
+
+define("LABEL_lcConfig","konfiguracja słownictwa");
+define("LABEL_lcDatos","metadane dla słownictwa");
+define("LABEL_Titulo","tytuł");
+define("FORM_LABEL_Titulo","_tytuł");
+define("FORM_LABEL_Autor","_autor");
+define("FORM_LABEL_URI","_URI");
+define("FORM_LABEL_Idioma","język");
+define("FORM_LABEL_FechaDia","dzień");
+define("FORM_LABEL_FechaMes","miesiąc");
+define("FORM_LABEL_FechaAno","rok");
+define("FORM_LABEL_Keywords","słowa kluczowe");
+define("FORM_LABEL_TipoLenguaje","typ_języka");
+define("FORM_LABEL_Cobertura","zakres");
+define("FORM_LABEL_Terminos","terminy");
+define("FORM_LABEL_RelTerminos","relacje pomiędzy terminami");
+define("FORM_LABEL_TerminosUP","terminy równoznaczne");
+define("FORM_LABEL_Guardar","Zapisz");
+define("LABEL_verDetalle","zobacz szczególy z ");
+define("LABEL_verTerminosLetra","zobacz terminy zaczynjące się z ");
+
+define("LABEL_NB","Nota bibliograficzna");
+define("LABEL_NH","Nota historyczna");
+define("LABEL_NA","Nota o zakresie");   /* version 0.9.1 */
+define("LABEL_NP","Nota prywatna"); /* version 0.9.1 */
+
+
+define("LABEL_EditorNota","Edytor not");
+define("LABEL_EditorNotaTermino","nota dla ");
+define("LABEL_tipoNota","typ noty");
+define("FORM_LABEL_tipoNota","typ_noty");
+define("LABEL_nota","nota");
+define("FORM_LABEL_nota","_nota");
+define("LABEL_EditarNota","Edytuj notę");
+define("LABEL_EliminarNota","Usuń notę");
+
+define("LABEL_OptimizarTablas","Optymalizuj tabele");
+define("LABEL_TotalZthesLine","eksportuje do Zthes");
+
+/* v 9.2 */
+define("LABEL_negrita","pogrubienie");
+define("LABEL_italica","italic");
+define("LABEL_subrayado","podkreślenie");
+define("LABEL_textarea","body notes");
+define("MSGL_relacionIlegal","Niepoprawna relacja pomiędzy terminami");
+
+
+/* v 9.3 */
+define("LABEL_fecha_modificacion","modyfikowany");
+define("LABEL_TotalUsuarios","Całkowita liczba użytkowników");
+define("LABEL_TotalTerminos","Całkowita liczba terminów");
+define("LABEL_ordenar","wybierz według");
+define("LABEL_auditoria","nadzór nad terminami");
+define("LABEL_dia","dzień");
+define("LABEL_mes","miesiąc");
+define("LABEL_ano","rok");
+define("LABEL_terminosRepetidos","zduplikowane terminy");
+define("MSG_noTerminosLibres","brak wolnych terminów");
+define("MSG_noTerminosRepetidos","brak zduplikowanych terminów");
+define("LABEL_TotalSkosLine","eksportuj do Skos-core");
+
+$MONTHS=array("01"=>Sty,
+              "02"=>Lut,
+              "03"=>Mar,
+              "04"=>Kwi,
+              "05"=>Maj,
+              "06"=>Cze,
+              "07"=>Lip,
+              "08"=>Sie,
+              "09"=>Wrz,
+              "10"=>Paź,
+              "11"=>Lis,
+              "12"=>Gru
+              );
+
+/* v 9.4 */
+define("LABEL_SI","TAK");
+define("LABEL_NO","NIE");
+define("FORM_LABEL_jeraquico","polihierarchiczny");
+define("LABEL_jeraquico","Polihierarchiczny");
+define("LABEL_terminoLibre","wolny termin");
+
+
+/* v 9.5 */
+define("LABEL_URL_busqueda","Znajdź %s w:");
+
+/* v 9.6 */
+define("LABEL_relacion_vocabulario","relación con otro vocabulario");
+define("FORM_LABEL_relacion_vocabulario","równowartość");
+define("FORM_LABEL_nombre_vocabulario","docelowe słownictwo");
+define("LABEL_vocabulario_referencia","docelowe słownictwo");
+define("LABEL_NO_vocabulario_referencia","brak docelowego słownictwa do utworzenia relacji dla terminologii");
+define("FORM_LABEL_tipo_equivalencia","typ równowartości");
+define("LABEL_vocabulario_principal","słownictwo");
+define("LABEL_tipo_vocabulario","typ");
+
+define("LABEL_termino_equivalente","równoważny");
+define("LABEL_termino_parcial_equivalente","częściowo równoważny");
+define("LABEL_termino_no_equivalente","nie równoważny");
+
+define("EQ_acronimo","EQ");
+define("EQP_acronimo","EQP");
+define("NEQ_acronimo","NEQ");
+define("LABEL_NC","Cataloger's note");
+
+define("LABEL_resultados_suplementarios","dodatkowe wyniki");
+define("LABEL_resultados_relacionados","powiązane wyniki");
+
+/* v 9.7 */
+define("LABEL_export","eksport");
+define("FORM_LABEL_format_export","wybierz schemat XML");
+
+
+/* v 1.0 */
+define("LABEL_fecha_creacion","utworzony");
+define("NB_acronimo","BN");
+define("NH_acronimo","HN");
+define("NA_acronimo","SN");
+define("NP_acronimo","PN");
+define("NC_acronimo","CN");
+
+define("LABEL_Candidato","proponowany termin");
+define("LABEL_Aceptado","zatwierdzony termin");
+define("LABEL_Rechazado","odrzucony termin");
+define("LABEL_Ultimos_aceptados","ostatnio zatwierdzone terminy");
+define("MSG_ERROR_ESTADO","niewłaściwy status");
+
+define("LABEL_Candidatos","proponowane terminy");
+define("LABEL_Aceptados","zatwierdzone terminy");
+define("LABEL_Rechazados","odrzucone terminy");
+
+define("LABEL_User_NoHabilitado","wyłącz");
+define("LABEL_User_Habilitado","włącz");
+
+// define("LABEL_CandidatearTermino","Candidatear término");
+define("LABEL_CandidatearTermino","proponowany termin");
+define("LABEL_AceptarTermino","zatwierdź termin");
+define("LABEL_RechazarTermino","odrzuć termin");
+
+
+/* v 1.01 */
+define("LABEL_TERMINO_SUGERIDO","chodziło o:");
+
+
+/* v 1.02 */
+define("LABEL_esSuperUsuario","jest administratorem");
+define("LABEL_Cancelar","anuluj");
+define("LABEL_Guardar","zapisz");
+
+
+/* v 1.033 */
+define("MENU_AgregarTEexist","Podporządkuj istniejący termin");
+define("MENU_AgregarUPexist","Przypisz istniejący równoważny termin");
+define("LABEL_existAgregarUP","Dodaj UF termin do %s");
+define("LABEL_existAgregarTE","Dodaj węższy termin do %s ");
+define("MSG_minCharSerarch","Wyszukiwane wyrażenie <i>%s</i> ma tylko <strong>%s </strong> znaków. Należy zwiększyć wartość do <strong>%s </strong> znaków");
+
+/* v 1.04 */
+define("LABEL_terminoExistente","istniejący termin");
+define("HELP_variosTerminos","Aby dodać jednocześnie wiele terminów należy je umieścić <strong>jako jedno słowo na linię</strong>.");
+
+
+/* v 1.05 */
+$idiomas_disponibles = array(
+     "ca"  => array("català", "", "ca"),
+     "cn"  => array("中文","", "cn"),
+     "de"  => array("deutsch","", "de"),
+     "en"  => array("english", "", "en"),
+     "es"  => array("español", "", "es"),
+     "fr"  => array("français","", "fr"),
+     "it"  => array("italiano","", "it"),
+     "nl"  => array("nederlands","", "nl"),
+     "pl"  => array("polski","", "pl"),    
+     "pt"  => array("portugüés","", "pt")
+    );
+
+/* Install messages */
+
+define("FORM","Formularz") ;
+define("ERROR","Błąd") ;
+define("LABEL_bienvenida","Witaj na Serwerze Słownictwa TemaTres") ;
+
+// COMMON SQL
+define("PARAM_SERVER","Adres serwera") ;
+define("PARAM_DBName","Nazwa bazy danych") ;
+define("PARAM_DBLogin","Użytkownik bazy danych") ;
+define("PARAM_DBPass","Hasło do bazy danych") ;
+define("PARAM_DBprefix","Prefix dla tabel") ;
+
+
+$install_message[101] = "Konfiguracja TemaTres" ;
+
+$install_message[201] = "Nie mogę odnaleźć pliku z konfiguracją połączenia do bazy danych (/include/db.tematres.php)." ;
+$install_message[202] = "Znaleziono plik z konfiguracją połączenia do bazy danych." ;
+$install_message[203] = "Nie udało się połączyć z serwerem bazy danych <em>%s</em> jako użytkownik <em>%s</em>. Proszę sprawdzić konfigurację (/include/db.tematres.php)." ;
+$install_message[204] = "Pomyślnie nawiązano połączenie z serwerem <em>%s</em>" ;
+$install_message[205] = "Nie udało się połączyć do bazy danych <em>%s</em> na serwerze <em>%s</em>. Proszę sprawdzić konfigurację (/include/db.tematres.php)." ;
+$install_message[206] = "Pomyślnie nawiązano połączenie z bazą danych <em>%s</em> na serwerze <em>%s</em>." ;
+
+$install_message[301] = "Ooppsss... Dla tej konfiguracji istnieje już instalacja TemaTres. Proszę sprawdzić konfigurację (/include/db.tematres.php)." ;
+$install_message[305] = " Sprawdzanie hasła." ;
+$install_message[306] = 'Konfiguracja zakończona pomyślnie, <a href="index.php">Miłego korzystania z Serwera TemaTres</a>' ;
+/* end Install messages */
+
+
+/* v 1.1 */
+define('MSG_ERROR_CODE',"niewłaściwy kod");
+define('LABEL_CODE',"kod");
+define('LABEL_Ver',"Pokaż");
+define('LABEL_OpcionesTermino',"Termin");
+define('LABEL_CambiarEstado',"Zmień status terminu");
+define('LABEL_ClickEditar',"Kliknij aby edytować...");
+define('LABEL_TopTerm',"Posiada ten top termin");
+define('LABEL_esFraseExacta',"dokładna fraza");
+define('LABEL_DesdeFecha',"Utworzone dokładnie lub później");
+define('LABEL_ProfundidadTermino',"jest umiejscowiony na poziomie");
+define('LABEL_esNoPreferido',"brak preferowanego terminu");
+define('LABEL_BusquedaAvanzada',"wyszukiwanie zaawansowane");
+define('LABEL_Todos',"wszystko");
+define('LABEL_QueBuscar',"jakie wyszukiwanie?");
+
+define("LABEL_import","importowanie") ;
+define("IMPORT_form_legend","importuj tezaurus z pliku txt wciętego tabulatorami") ;
+define("IMPORT_form_label","plik") ;
+define("IMPORT_file_already_exists","plik txt już istnieje na serwerze") ;
+define("IMPORT_file_not_exists","jeszcze nie zaimportowano pliku txt") ;
+define("IMPORT_do_it","Można rozpocząć importowanie") ;
+define("IMPORT_working","trwa importowanie") ;
+define("IMPORT_finish","importowanie zakończone") ;
+define("LABEL_reIndice","odtwórz indeksy") ;
+define("LABEL_dbMantenimiento","utrzymywanie bazy danych");
+
+
+/*
+v 1.2
+*/
+define('LABEL_relacion_vocabularioWebService',"relation with term from remote target vocabulary");
+define('LABEL_vocabulario_referenciaWS',"remote target vocabulary (web  services)");
+define('LABEL_TargetVocabularyWS',"remote target vocabulary (web  services)");
+define('LABEL_tvocab_label',"label for the reference");
+define('LABEL_tvocab_tag',"tag for the reference");
+define('LABEL_tvocab_uri_service',"URL for the web services reference");
+define('LABEL_targetTermsforUpdate',"terms with pending update");
+define('LABEL_ShowTargetTermsforUpdate',"check terms update");
+define('LABEL_enable',"enable");
+define('LABEL_disable',"disable");
+define('LABEL_notFound',"term not found");
+define('LABEL_termUpdated',"term updated");
+define('LABEL_ShowTargetTermforUpdate',"update");
+define('LABEL_relbetweenVocabularies',"relations between vocabularies");
+
+;?>
