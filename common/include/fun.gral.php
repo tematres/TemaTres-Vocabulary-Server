1b3cf8a8e708558f2b01466e9ae3a602f0ae7da0
diego <tematres@r020.com.ar>
2020-07-14 10:10:55 -0300

    remove magic_quotes

--- common/include/fun.gral.php
+++ common/include/fun.gral.php
@@ -24,48 +24,46 @@ $_GET=XSSpreventArray($_GET);
 function PHP_magic_quotes()
 {
 
-    if (!get_magic_quotes_gpc()) {
-        if (is_array($_GET)) {
-            while (list($k, $v) = each($_GET)) {
-                if (is_array($_GET[$k])) {
-                    while (list($k2, $v2) = each($_GET[$k])) {
-                        $_GET[$k][$k2] = addslashes($v2);
-                    }
-                        @reset($_GET[$k]);
-                } else {
-                        $_GET[$k] = addslashes($v);
+    if (is_array($_GET)) {
+        while (list($k, $v) = each($_GET)) {
+            if (is_array($_GET[$k])) {
+                while (list($k2, $v2) = each($_GET[$k])) {
+                    $_GET[$k][$k2] = addslashes($v2);
                 }
+                    @reset($_GET[$k]);
+            } else {
+                    $_GET[$k] = addslashes($v);
             }
-                @reset($_GET);
         }
+            @reset($_GET);
+    }
 
-        if (is_array($_POST)) {
-            while (list($k, $v) = each($_POST)) {
-                if (is_array($_POST[$k])) {
-                    while (list($k2, $v2) = each($_POST[$k])) {
-                        $_POST[$k][$k2] = addslashes($v2);
-                    }
-                        @reset($_POST[$k]);
-                } else {
-                        $_POST[$k] = addslashes($v);
+    if (is_array($_POST)) {
+        while (list($k, $v) = each($_POST)) {
+            if (is_array($_POST[$k])) {
+                while (list($k2, $v2) = each($_POST[$k])) {
+                    $_POST[$k][$k2] = addslashes($v2);
                 }
+                    @reset($_POST[$k]);
+            } else {
+                    $_POST[$k] = addslashes($v);
             }
-                @reset($_POST);
         }
+            @reset($_POST);
+    }
 
-        if (is_array($HTTP_COOKIE_VARS)) {
-            while (list($k, $v) = each($HTTP_COOKIE_VARS)) {
-                if (is_array($HTTP_COOKIE_VARS[$k])) {
-                    while (list($k2, $v2) = each($HTTP_COOKIE_VARS[$k])) {
-                        $HTTP_COOKIE_VARS[$k][$k2] = addslashes($v2);
-                    }
-                        @reset($HTTP_COOKIE_VARS[$k]);
-                } else {
-                        $HTTP_COOKIE_VARS[$k] = addslashes($v);
+    if (is_array($HTTP_COOKIE_VARS)) {
+        while (list($k, $v) = each($HTTP_COOKIE_VARS)) {
+            if (is_array($HTTP_COOKIE_VARS[$k])) {
+                while (list($k2, $v2) = each($HTTP_COOKIE_VARS[$k])) {
+                    $HTTP_COOKIE_VARS[$k][$k2] = addslashes($v2);
                 }
+                    @reset($HTTP_COOKIE_VARS[$k]);
+            } else {
+                    $HTTP_COOKIE_VARS[$k] = addslashes($v);
             }
-                @reset($HTTP_COOKIE_VARS);
         }
+            @reset($HTTP_COOKIE_VARS);
     }
 }
 
