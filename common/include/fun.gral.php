d0a3da88f55da66320772c6a9a701fb88fc586fd
tematres <tematres@r020.com.ar>
2020-09-19 14:55:26 -0300

    fix order of condition for 7.4 complaince

--- common/include/fun.gral.php
+++ common/include/fun.gral.php
@@ -1466,7 +1466,7 @@ function isValidLetter($string)
 // Return base URL of the current URL or instance of vocabulary
 function getURLbase()
 {
-    $s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+    $s = empty($_SERVER["HTTPS"]) ? '' : (($_SERVER["HTTPS"] == "on") ? "s" : "");
     $protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
     $port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
     $uri = $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
