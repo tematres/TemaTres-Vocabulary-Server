2fdf9bfeb98ad6dd433459419a6e5ef9f87d300a
diego <tematres@r020.com.ar>
2022-03-22 10:44:58 -0300

    fix bug to enable to use CDN bootstrap

--- common/include/fun.html.php
+++ common/include/fun.html.php
@@ -2018,6 +2018,7 @@ function makeGlossary($notesType = array("NA"), $params = array())
 
 function HTMLheader($metadata)
 {
+GLOBAL $CFG;
 
     $rows='<meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1">';
     //    <!-- Latest compiled and minified CSS -->
